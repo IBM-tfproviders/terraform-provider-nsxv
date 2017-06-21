459e166b082cf4cd87c57b3d76d37e3f6cd1b51d
Sanjay Verma <sanverm2@in.ibm.com>
2017-06-21 16:53:29 +0530

    Added support for NSXv Edge Logical Router Interface Update

--- nsx/resource_nsx_edge_dlr.go
+++ nsx/resource_nsx_edge_dlr.go
@@ -51,7 +51,7 @@ func resourceNsxEdgeDLR() *schema.Resource {
 				Computed: true,
                         },
 			"interface": &schema.Schema{
-				Type:     schema.TypeList,
+				Type:     schema.TypeSet,
 				Required: true,
 				MinItems: 1,
 				MaxItems: 999,
@@ -172,7 +172,8 @@ func resourceNsxEdgeDLRInterfaceRead(d *schema.ResourceData, meta interface{}) e
 
 		if edgeType != EdgeTypeDistributedRouter {
 			log.Printf("[ERROR] Edge type is not ", EdgeTypeDistributedRouter)
-			err := fmt.Errorf("[ERROR] Only Edge type %s is supported for this operation",
+			err := fmt.Errorf(
+				"[ERROR] Only Edge type %s is supported for this operation",
 				EdgeTypeDistributedRouter)
 			return err
 		}
@@ -193,22 +194,120 @@ func resourceNsxEdgeDLRInterfaceRead(d *schema.ResourceData, meta interface{}) e
 		}
 
 		d.Set("type", EdgeTypeDistributedRouter)
-       }
+	}
 
 	log.Printf("[INFO] Read NSX Edge Router Interface: ", edgeId)
-	_, err := dlrInterfaces.Get(edgeId)
+	resp, err := dlrInterfaces.Get(edgeId)
 
 	if err != nil {
-		log.Printf("[ERROR] Retriving Edge '%s' failed with error : '%v'", edgeId, err)
+		log.Printf("[ERROR] Retriving Edge Interfaces %s failed with error : '%v'", edgeId, err)
 		d.SetId("")
 		return err
+	}
+
+	log.Printf("[DEBUG] Retrieved Edge Interfaces %v", resp) 
+
+	ifaces := make([]map[string]interface{}, 0)
+
+	for _, curIface := range resp.EdgeDLRInterfaceList {
+
+		if prvIfaces, ok := d.Get("interface").(*schema.Set); ok {
+
+		 	for _, val := range prvIfaces.List() {
+				prvIface := val.(map[string]interface{})
+				
+				log.Printf("[DEBUG] curIface.ConnectedToName %s,prvIface[logical_switch_id] %s\n",
+					curIface.ConnectedToName,
+					prvIface["logical_switch_id"].(string))
+
+				log.Printf("[DEBUG] curIface.AddressGroups[0].PrimaryAddress %s, prvIface[ip] %s\n",
+					curIface.AddressGroups[0].PrimaryAddress,
+					prvIface["ip"].(string) )
+
+				if curIface.Name == prvIface["name"] &&
+					curIface.ConnectedToId == prvIface["logical_switch_id"].(string) &&
+					curIface.AddressGroups[0].PrimaryAddress == prvIface["ip"].(string) {
+
+						prvIface["index"] = curIface.Index
+						ifaces = append(ifaces, prvIface)
+						log.Printf("[DEBUG] Updated index %s", 
+							curIface.Index)
+						break
+				}
+			}
+		}
+	}
+
+	err = d.Set("interface", ifaces)
+	if err != nil {
+		return fmt.Errorf("Invalid interfaces to set: %#v", ifaces)
 	}
 	return nil
 }
 
 func resourceNsxEdgeDLRInterfaceUpdate(d *schema.ResourceData, meta interface{}) error {
-	log.Printf("[INFO] Update NSX Edge Router Interface TBD")
-	return nil
+	client := meta.(*govnsx.Client)
+        edlrIface := nsxresource.NewEdgeDLRInterfaces(client)
+
+        edgeId := d.Get("edge_id").(string)
+
+	if d.HasChange("interface") {
+
+		oldIface, newIface := d.GetChange("interface")
+
+		oldIfaceSet := oldIface.(*schema.Set)
+		newIfaceSet := newIface.(*schema.Set)
+
+		addedIfaceSet := newIfaceSet.Difference(oldIfaceSet)
+		removedIfaceSet := oldIfaceSet.Difference(newIfaceSet)
+		
+		log.Printf("[DEBUG] added Interface : %#v\n", addedIfaceSet)
+               	log.Printf("[DEBUG] removed Interface : %#v\n", removedIfaceSet)	
+		
+		for _, removedIfaceRaw := range removedIfaceSet.List() {
+			removedIface  := removedIfaceRaw.(map[string]interface{})	
+			removedIfaceIndex := removedIface["index"].(string)
+			err := edlrIface.Delete(edgeId, removedIfaceIndex)
+			if err != nil {
+				log.Printf("[Error]  NSX Edge Interface Delete returned error : %v", err)
+				return err
+			}
+		}
+	
+		ifaces := []nsxtypes.EdgeDLRInterface{}
+		for _, addedIfaceRaw := range addedIfaceSet.List() {
+			addedIface := addedIfaceRaw.(map[string]interface{})
+
+			addrGroups := []nsxtypes.AddressGroup{nsxtypes.AddressGroup{
+                        	PrimaryAddress: addedIface["ip"].(string),
+                        	SubnetMask:     addedIface["mask"].(string),
+				},
+			}	
+			
+			iface := nsxtypes.EdgeDLRInterface{
+				AddressGroups: addrGroups,
+				Name:          addedIface["name"].(string),
+				ConnectedToId: addedIface["logical_switch_id"].(string),
+				Type:          "internal",
+				IsConnected:   true,
+			}
+
+			ifaces = append(ifaces, iface)	
+		}
+			
+		if len(ifaces) > 0 {
+			addInterfacesSpec := &nsxtypes.EdgeDLRAddInterfacesSpec{
+				EdgeDLRInterfaceList: ifaces,
+			}
+
+			_, err := edlrIface.Post(addInterfacesSpec, edgeId)
+			if err != nil {
+				log.Printf("[ERROR] dlrInterfaces.Post () returned error : %v", err)
+			return err
+			}
+		}
+        }
+	return resourceNsxEdgeDLRInterfaceRead(d, meta)
 }
 
 func resourceNsxEdgeDLRInterfaceDelete(d *schema.ResourceData, meta interface{}) error {
@@ -235,16 +334,18 @@ func parseAndValidateDLRResourceData(d *schema.ResourceData, meta interface{}) (
 	ifCfgs := []ifCfg{}
 	vL := d.Get("interface")
 
-	for _, value := range vL.([]interface{}) {
+	if ifSet, ok := vL.(*schema.Set); ok {
+		for _, value := range ifSet.List() {
 
-		newInterface := ifCfg{}
-		iface := value.(map[string]interface{})
+			newInterface := ifCfg{}
+			iface := value.(map[string]interface{})
 
-		newInterface.name = iface["name"].(string)
-		newInterface.ip = iface["ip"].(string)
-		newInterface.mask = iface["mask"].(string)
-		newInterface.logical_switch_id = iface["logical_switch_id"].(string)
-		ifCfgs = append(ifCfgs, newInterface)
+			newInterface.name = iface["name"].(string)
+			newInterface.ip = iface["ip"].(string)
+			newInterface.mask = iface["mask"].(string)
+			newInterface.logical_switch_id = iface["logical_switch_id"].(string)
+			ifCfgs = append(ifCfgs, newInterface)
+		}
 	}
 	dlr.ifCfgList = ifCfgs
 	return dlr, nil
